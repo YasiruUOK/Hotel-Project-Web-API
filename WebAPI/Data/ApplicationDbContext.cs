843b962a53ab5c760163556531765266c5a41509
Yasiru Kavinda <yasirukavinda89@gmail.com>
2022-01-16 08:15:35 +0530

    Add project files.

---
+++ WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() :
+          base("localNetwork")
+        {
+        }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        public DbSet<NewsFeed> newsFeed { get; set; }
+    }
+}
---
+++ WebAPI/Models/NewsFeed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class NewsFeed
+    {
+        public int ID { get; set; }
+        public string Topic { get; set; }
+        public string News { get; set; }
+        public string AddedDate { get; set; }
+        public string Image_URL { get; set; }
+    }
+}
